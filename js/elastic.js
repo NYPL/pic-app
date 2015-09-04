0b375ab1cdad91e05f20add7e6b4d3247818878d
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-09-04 15:18:44 -0400

    :space_invader: first test with csv-powered data

--- js/elastic.js
+++ js/elastic.js
@@ -1,19 +1,93 @@
 var r = new XMLHttpRequest();
 
+var handler = new Cesium.ScreenSpaceEventHandler(canvas);
+
+var pickedEntities = [];
+
+var tooltip = document.getElementById("tooltip");
+
+function clearPicked() {
+    var entity;
+    while (pickedEntities.length>0) {
+        entity = pickedEntities.splice(0, 1)[0];
+        entity.primitive.color = new Cesium.Color(1, 0.01, 0.01, 1);
+    }
+}
+
+function initMouseHandler(handler) {
+    canvas.setAttribute('tabindex', '0'); // needed to put focus on the canvas
+    canvas.onclick = function() {
+        canvas.focus();
+    };
+
+    // var tooltip = viewer.entities.add({
+    //     label : {
+    //         show : false
+    //     }
+    // });
+
+    var ellipsoid = scene.globe.ellipsoid;
+
+    handler.setInputAction(function(movement) {
+        // console.log(movement);
+        // flags.looking = true;
+        // mousePosition = startMousePosition = Cesium.Cartesian3.clone(movement.position);
+    }, Cesium.ScreenSpaceEventType.LEFT_DOWN);
+
+    handler.setInputAction(function(movement) {
+        clearPicked();
+        // pick
+        var pickedObject = scene.pick(movement.endPosition);
+        if (Cesium.defined(pickedObject) && (pickedObject.id.toString().indexOf("P_") === 0)) {
+            pickedEntities.push(pickedObject);
+            // console.log("thing:", pickedObject.primitive);
+            // console.log("first:", pickedObject.color);
+            pickedObject.primitive.color = new Cesium.Color(1, 1, 0.01, 1);
+            // console.log("then:", pickedObject.color);
+            // label tooltip
+            var cartesian = viewer.camera.pickEllipsoid(movement.endPosition, ellipsoid);
+            if (cartesian) {
+                var cartographic = ellipsoid.cartesianToCartographic(cartesian);
+                var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(4);
+                var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(4);
+
+                // tooltip.position = cartesian;
+                // tooltip.label.show = true;
+                // tooltip.label.text = '(' + longitudeString + ', ' + latitudeString + ')';
+                // tooltip.label.text = pickedObject.id;
+
+                tooltip.innerHTML = "<p>ID:" + pickedObject.id + "</p>";
+            }
+        } else {
+            tooltip.innerHTML = "";
+        }
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+    handler.setInputAction(function(position) {
+        // console.log(position);
+        // flags.looking = false;
+    }, Cesium.ScreenSpaceEventType.LEFT_UP);
+}
+
+initMouseHandler(handler);
+
 var globe_data;
 
-r.open("GET", "latlons.txt", true);
+r.open("GET", "csv/latlons.txt", true);
 
 r.onreadystatechange = function () {
   if (r.readyState != 4 || r.status != 200) return;
-  globe_data = JSON.parse(r.responseText)[0][1];
+  globe_data = JSON.parse(r.responseText)[1];
   var i, l=globe_data.length;
   for (var i=0; i<l; i=i+3) {
-    points.add({
-      position : new Cesium.Cartesian3.fromDegrees(globe_data[i+1], globe_data[i]),
-      color : new Cesium.Color(1, 0.01, 0.01, 1),
-      pixelSize : 1.5
-    });
+      var p = points.add({
+          id: "P_"+globe_data[i+2],
+          name: "ConstituentID: " + globe_data[i+2],
+          position : new Cesium.Cartesian3.fromDegrees(globe_data[i+1], globe_data[i]),
+          color: new Cesium.Color(1, 0.01, 0.01, 1),
+          pixelSize : 2,
+          scaleByDistance : new Cesium.NearFarScalar(2.0e2, 3, 8.0e5, 1)
+      });
   }
 };
 r.send(null);
@@ -30,7 +104,7 @@ function updatePoints(newPoints) {
     points.add({
       position : new Cesium.Cartesian3.fromDegrees(point[0], point[1]),
       color : new Cesium.Color(1, 0.01, 0.01, 1),
-      pixelSize : 1.5
+      pixelSize : 1.5,
     });
   }
 }

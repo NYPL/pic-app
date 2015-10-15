a6c46f3292722984712e4faa1e2afc2b74d7bf00
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-10-15 19:28:14 -0400

    fixed prebuild

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -13,11 +13,6 @@ git config user.email "travis"
 
 # git checkout csv
 
-# echo ""
-# echo "Pulling..."
-
-# git pull origin csv -m ":rocket: merge from travis-ci"
-
 python ./python/index_builder.py
 
 echo ""
@@ -30,11 +25,14 @@ git add csv
 git commit -m ":rocket: new deploy from travis-ci"
 
 echo ""
+echo "Pulling latest gh-pages..."
+
+git pull origin gh-pages -m ":rocket: merge from travis-ci"
+
+echo ""
 echo "Pushing..."
 
 git push $REPO_URL gh-pages
-
-git push $REPO_URL master:gh-pages
 
 echo ""
 echo "### DEPLOY COMPLETE"

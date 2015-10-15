31f7ea1ef17f73f32613185eec924d1986451128
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-10-15 14:30:12 -0400

    :space_invader: testing deploy with travis

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 echo "pushing to $GH_REPO [via travis]"
-export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
-git branch -a
+
+REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
+
+git remote add origin $REPO_URL
+
+git clone $REPO_URL
+
+git pull $REPO_URL csv -m ":rocket: merge new csvs"
+
+python ./python/index_builder.py
+
 echo "STATUS"
 git status
-echo "remotes pre pre-authorized remote url"
-git remote add origin $REPO_URL
-git clone $REPO_URL
-git pull origin csv -m ":rocket: merge new csvs"
-python python/index_builder.py
+
 git commit -am ":rocket: new deploy from travis-ci"
-git push origin gh-pages
+
+git push $REPO_URL gh-pages

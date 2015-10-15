edd7e0f03d80bc52951af6a3d5683323fbe9244a
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-10-15 14:04:18 -0400

    :space_invader: added python code to try with travis

---
+++ python/prebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "pushing to $GH_REPO [via travis]"
+export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
+git branch -a
+echo "STATUS"
+git status
+echo "remotes pre pre-authorized remote url"
+git remote add origin $REPO_URL
+git clone $REPO_URL
+python index_builder.py
+git commit -am ":rocket: new deploy from travis-ci"
+git push origin gh-pages

bfa800ab3c214644764b3dc940fa631378cadc6a
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-10-15 20:38:42 -0400

    trying more......

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -4,51 +4,48 @@ echo ""
 echo "#### STARTED TRAVIS MAGIC ON $GH_REPO"
 
 export REPO=$(git config remote.origin.url)
-export BRANCH="gh-pages"
 export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
 
 git config --global user.name "travis-bot"
 git config --global user.email "travis"
 
+echo ""
+echo "#### STATUS"
+echo "Branch"
+git branch -a
+
+echo ""
+echo "Status"
+git status
+
+# echo ""
+# echo "rename"
+# git remote rename origin old
+
+# echo ""
+# echo "new origin"
+# git remote add origin $REPO_URL
+# git config remote.origin.url $REPO_URL
+
+git checkout origin/csv
+
+# echo ""
+# echo "new branches:"
+# git branch -a
+
+echo ""
+echo "Run the index"
 # python ./python/index_builder.py
 
 echo ""
-echo "#### STATUS"
-echo "branch"
-git branch -a
-
-echo ""
-echo "status"
-git status
-
-echo ""
-echo "rename"
-git remote rename origin old
-
-echo ""
-echo "new origin"
-git remote add origin $REPO_URL
-git config remote.origin.url $REPO_URL
-git checkout origin/$BRANCH
-
-echo ""
-echo "new branches:"
-git branch -a
-
-echo ""
-echo "adding csv"
+echo "Adding csv"
 git add csv
 
 git commit -m ":rocket: new deploy from travis-ci"
 
 echo ""
-echo "Pulling latest gh-pages..."
-
-git branch $BRANCH origin/$BRANCH
-
-echo ""
 echo "Checkout"
-git checkout $BRANCH
+git checkout origin/gh-pages
 
 echo ""
 echo "Merge"
@@ -57,7 +54,7 @@ git merge csv -m ":rocket: merge from travis-ci"
 echo ""
 echo "Push"
 
-git push origin $BRANCH
+git push origin gh-pages
 
 echo ""
 echo "#### DEPLOY COMPLETE"

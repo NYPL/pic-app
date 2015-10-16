99f02fc8078c18bebdec7fa9aad829f5f95afde7
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-10-15 20:18:06 -0400

    trying more.

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -17,21 +17,27 @@ echo "#### STATUS"
 echo "branch"
 git branch -a
 
+echo ""
 echo "status"
 git status
 
+echo ""
 echo "rename"
 git remote rename origin old
 
+echo ""
 echo "-v"
 git remote -v
 
+echo ""
 echo "new origin"
 git remote add origin $REPO_URL
 
+echo ""
 echo "config"
 git config remote.origin.url $REPO_URL
 
+echo ""
 echo "adding csv"
 git add csv
 
@@ -41,13 +47,17 @@ echo ""
 echo "Pulling latest gh-pages..."
 
 git branch $BRANCH origin/$BRANCH
-echo "  Checkout"
+
+echo ""
+echo "Checkout"
 git checkout $BRANCH
-echo "  Merge"
+
+echo ""
+echo "Merge"
 git merge csv -m ":rocket: merge from travis-ci"
 
 echo ""
-echo "  Push"
+echo "Push"
 
 git push origin $BRANCH
 

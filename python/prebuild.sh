7cf9c65d9ec3940a56347fd8413852b04b6d7787
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-10-15 20:06:44 -0400

    trying more

--- python/prebuild.sh
+++ python/prebuild.sh
@@ -3,6 +3,8 @@
 echo ""
 echo "#### STARTED TRAVIS MAGIC ON $GH_REPO"
 
+REPO=$(git config remote.origin.url)
+BRANCH="gh-pages"
 REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
 
 git config user.name "travis-bot"
@@ -22,12 +24,14 @@ git commit -m ":rocket: new deploy from travis-ci"
 echo ""
 echo "Pulling latest gh-pages..."
 
-git checkout origin/gh-pages -m ":rocket: merge from travis-ci"
+git checkout $BRANCH
+
+git merge csv -m ":rocket: merge from travis-ci"
 
 echo ""
 echo "Pushing..."
 
-git push $REPO_URL gh-pages
+git push $REPO $BRANCH
 
 echo ""
 echo "#### DEPLOY COMPLETE"

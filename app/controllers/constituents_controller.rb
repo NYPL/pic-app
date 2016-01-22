7e8fa28a5d4e6ab24becede8d8623cd312e68a3e
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-01-22 10:04:02 -0500

    :space_invader: only using source of constituent

--- app/controllers/constituents_controller.rb
+++ app/controllers/constituents_controller.rb
@@ -6,9 +6,12 @@ class ConstituentsController < ApplicationController
     connection_string = "https://#{ENV['ELASTIC_USER']}:#{ENV['ELASTIC_PASSWORD']}@#{ENV['ELASTIC_HOST']}"
     client = Elasticsearch::Client.new host: connection_string
     begin
-      @constituent = client.search index: 'pic', q: "constituent.ConstituentID:#{params[:id]}"
+      results = client.search index: 'pic', q: "constituent.ConstituentID:#{params[:id]}"
     rescue
       @constituent = nil
+    end
+    if results && results["hits"]["total"] > 0
+      @constituent = results["hits"]["hits"][0]["_source"]
     end
     respond_with @constituent do |f|
       f.html

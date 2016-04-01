bfe34a07f7f5188a5eade70787b3625c15e083f8
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2016-04-01 12:01:08 -0400

    :space_invader: removed anti caching js

--- app/assets/javascripts/PIC.ts
+++ app/assets/javascripts/PIC.ts
@@ -687,7 +687,8 @@ module PIC {
         }
 
         loadBaseData () {
-            this.loadTextFile(this.rootPath + "csv/latlons.txt?i=" + Math.round(Math.random() * 100000000), function (responseText) {
+            // this.loadTextFile(this.rootPath + "csv/latlons.txt?i=" + Math.round(Math.random() * 100000000), function (responseText) {
+            this.loadTextFile(this.rootPath + "csv/latlons.txt", function (responseText) {
                 var baseData = JSON.parse(responseText)[1];
                 this.parseBaseData(baseData);
             });
@@ -712,7 +713,8 @@ module PIC {
                 this.allIDs.push(id);
             }
 
-            this.loadTextFile(this.rootPath + "csv/heights.txt?i=" + Math.round(Math.random() * 100000000), function(responseText) {
+            // this.loadTextFile(this.rootPath + "csv/heights.txt?i=" + Math.round(Math.random() * 100000000), function(responseText) {
+            this.loadTextFile(this.rootPath + "csv/heights.txt", function(responseText) {
                 var heightData = JSON.parse(responseText)[1];
                 this.parseHeightData(heightData);
             });
@@ -1677,7 +1679,8 @@ module PIC {
 
             if (facet[0] !== "bbox") {
                 // hack for ignoring the bbox (has no csv)
-                var url = this.rootPath + "csv/" + facet[0] + ".csv?i=" + Math.round(Math.random() * 100000000);
+                // var url = this.rootPath + "csv/" + facet[0] + ".csv?i=" + Math.round(Math.random() * 100000000);
+                var url = this.rootPath + "csv/" + facet[0] + ".csv";
                 this.loadTextFile(url, this.updateFacet, facet);
             } else {
                 this.initFacetWidget(widget);
